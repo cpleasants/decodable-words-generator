23d56d482ab41ae3bec731262af40bb9d3412fa9
cpleasants <chris.pleasants@generalassemb.ly>
2025-04-06 22:22:03 -0400

    modified dictionary to come from childrens text and CHILDE data

--- decodable_words_generator/constants.py
+++ decodable_words_generator/constants.py
@@ -1,5 +1,2 @@
 """Constants used throughout the data generation process."""
-
-MAX_DECODABLE_RANK = 20_000 # Maximum word frequency rank for _decodable_ words
-MAX_UNDECODABLE_RANK = 2_000 # Maximum word frequency rank for _undecodable_ words
-VOWELS = {'a', 'e', 'i', 'o', 'u'}
+TOP_N = 10_000 # Top N most frequent words from the corpus
---
+++ decodable_words_generator/top_n.py
@@ -0,0 +1,73 @@
+import gzip   # To read .gz compressed files
+import nltk   # Natural Language Toolkit for word frequency analysis
+
+def tokenize_clean_text(fname):
+    """
+    Reads a gzip-compressed text file, tokenizes the words,
+    converts them to lowercase, and filters out tokens that
+    don't start and end with alphabetic characters. Note that
+    the data used in this project is highly cleaned, such as
+    punctuation already having been separated from the words.
+
+    Args:
+        fname (str): Path to the .gz file
+
+    Returns:
+        list: A list of cleaned, lowercase words
+    """
+    words = []
+    with gzip.open(fname, 'rt') as f:
+        for line in f.readlines():
+            # Split line into words, lowercase them, and filter
+            words += [
+                wrd.lower()
+                for wrd in line.split(' ')
+                if len(wrd) > 0 and wrd[0].isalpha() and wrd[-1].isalpha()
+            ]
+    return words
+
+def tokenize_files(files_list):
+    """
+    Tokenizes and cleans words from multiple compressed files.
+
+    Args:
+        files_list (list): List of .gz file paths
+
+    Returns:
+        list: Combined list of cleaned words from all files
+    """
+    words = []
+    for f in files_list:
+        words.extend(tokenize_clean_text(f))
+    return words
+
+def topn(words_list, n=10000):
+    """
+    Returns the top-N most frequent words as a list.
+
+    Args:
+        words_list (list): A list of words
+        n (int): Number of top words to include
+
+    Returns:
+        set: Set of top-N frequent words
+    """
+    fdist = nltk.FreqDist(words_list)
+    return [x[0] for x in fdist.most_common(n)]
+
+def generate_topn(n=10000, files=None):
+    """
+    High-level function to generate a top-N word set from a list of files.
+
+    Args:
+        n (int): Number of top words to return
+        files (list, optional): List of .gz file paths. Defaults to preset list.
+
+    Returns:
+        set: Set of top-N frequent words
+    """
+    if files is None:
+        files = ['data/aochildes.train.gz', 'data/cbt.train.gz', 'data/children_stories.train.gz']
+
+    words = tokenize_files(files)
+    return topn(words, n)
